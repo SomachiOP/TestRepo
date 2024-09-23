949f5c0d95ce21f44a5fba4ff9549a050dd41bc2
Amodh <amodh1257@gmail.com>
2024-09-22 23:17:17 -0400

    Added blam functionality

--- Test Space.py
+++ Test Space.py
@@ -1,4 +1,22 @@
+def blam(name: str = "?") -> str:
+    """
+    Function to blamify something
+
+    Args:
+        name (str, optional): Thing to blamify. Defaults to "?".
+
+    Returns:
+        str: Status of blamification
+    """
+    
+    return f"{name} has been blammed!"
+
+
+
 print("Hello World")
+print(blam("Python"))
+
+
 
 try:
   if int(input()) == 1:
